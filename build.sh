a3ab0d2cdada2c0d7b48dcaf622c5765087e96ea
mbenezra <mbenezra@users.noreply.github.com>
2020-01-05 10:54:04 +0100

    added

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t udpipe/server-da .
 docker save udpipe/server-da > udpipe-server-da.rar
-zip udpipe-server-da.rar.zip udpipe-server-da.rar
-zip -s=1000m udpipe-server-da.rar.zip --out udpipe-server-da.rar.split.zip
+#zip udpipe-server-da.rar.zip udpipe-server-da.rar
+#zip -s=1000m udpipe-server-da.rar.zip --out udpipe-server-da.rar.split.zip
